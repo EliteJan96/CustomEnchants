ffe2a2ae4efed24159cd9dc3e5cded3e86890223
Searchndstroy <searchndstroy@outlook.com>
2014-05-13 21:56:01 -0700

    Added license notice for GPL v 3

---
+++ src/io/github/searchndstroy/customenchnats/common/RomanNumeralCoverter.java
@@ -0,0 +1,81 @@
+/*
+ *     This file is part of CustomEnchants.
+
+    CustomEnchants is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    CustomEnchants is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with CustomEnchants.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.searchndstroy.customenchnats.common;
+
+public class RomanNumeralCoverter {
+
+	public static String IntegerToRomanNumeral(int input) {
+		if (input < 1 || input > 3999)
+			return "Invalid Roman Number Value";
+		String s = "";
+		while (input >= 1000) {
+			s += "M";
+			input -= 1000;
+		}
+		while (input >= 900) {
+			s += "CM";
+			input -= 900;
+		}
+		while (input >= 500) {
+			s += "D";
+			input -= 500;
+		}
+		while (input >= 400) {
+			s += "CD";
+			input -= 400;
+		}
+		while (input >= 100) {
+			s += "C";
+			input -= 100;
+		}
+		while (input >= 90) {
+			s += "XC";
+			input -= 90;
+		}
+		while (input >= 50) {
+			s += "L";
+			input -= 50;
+		}
+		while (input >= 40) {
+			s += "XL";
+			input -= 40;
+		}
+		while (input >= 10) {
+			s += "X";
+			input -= 10;
+		}
+		while (input >= 9) {
+			s += "IX";
+			input -= 9;
+		}
+		while (input >= 5) {
+			s += "V";
+			input -= 5;
+		}
+		while (input >= 4) {
+			s += "IV";
+			input -= 4;
+		}
+		while (input >= 1) {
+			s += "I";
+			input -= 1;
+		}
+		return s;
+	}
+
+}
